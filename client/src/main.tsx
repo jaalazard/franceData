f4a13dc20e01d8c135591fc142589d63d0773b7e
jaalazard <ja.alazard@proton.me>
2023-12-29 14:34:39 +0100

    registration done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import router from './router.tsx'
 import './main.css'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 require ('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const router = require('./router');
-const port = process.env.APP_PORT;
-app.use(router);
 app.use(express.json());
+const router = require('./routes/router');
+const port = process.env.APP_PORT;
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }));
+app.use(router);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
