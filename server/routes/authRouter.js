845d76fc80a921e912c742eeca5cb39d64351608
jaalazard <ja.alazard@proton.me>
2023-12-31 16:28:44 +0100

    logout done

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcrypt");
 const jose = require('jose')
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 const SECRET = process.env.JWT_SECRET;
+
 authRouter.get("/check", async (req, res) => {
   const jwt = req.cookies.token;
   if (jwt === undefined) {
@@ -53,7 +54,7 @@ authRouter.post("/login", async (req, res) => {
      .setIssuedAt()
      .setIssuer("http://localhost")
      .setAudience("http://localhost")
-     .setExpirationTime("24h")
+     .setExpirationTime("2h")
      .sign(new TextEncoder().encode(SECRET));
      res.cookie("token", jwt, {
        httpOnly: true,
@@ -71,6 +72,16 @@ authRouter.post("/login", async (req, res) => {
      console.error(error);
      res.status(500).json({ error: "Internal server error" });
    }
+  });
+
+  authRouter.post("/logout", async (req, res) => {
+    res.cookie("token", "", {
+      httpOnly: true,
+      secure: IS_PRODUCTION,
+      sameSite: "lax",
+      expires: new Date(0)
+    });
+    res.status(200).send('Déconnecté avec succès');
   });
   
   module.exports = authRouter;
